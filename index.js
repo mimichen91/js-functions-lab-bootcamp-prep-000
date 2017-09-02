ce815b35647169aa837d4ecf705bf9e0107ccbc0
Mimi Chen <yangminchen91@gmail.com>
2017-09-02 19:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo() {
-  return 'Happy holidays, ${name}!'
+function happyHolidayTo(holiday, name) {
+  return "Happy ${holiday}, ${name}!"
 }
 
 function holidayCountdown() {
